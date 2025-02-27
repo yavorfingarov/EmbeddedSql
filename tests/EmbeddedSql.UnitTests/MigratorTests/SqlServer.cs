40fa8b2ee29026796c411290882d014b049ef148
Yavor Fingarov <yavor.fingarov@outlook.com>
2025-02-27 12:25:43 +0200

    Removed sql server tests

--- tests/EmbeddedSql.UnitTests/MigratorTests/SqlServer.cs
+++ tests/EmbeddedSql.UnitTests/MigratorTests/SqlServer.cs
@@ -3,27 +3,30 @@ using Testcontainers.MsSql;
 
 namespace EmbeddedSql.UnitTests.MigratorTests
 {
-    [Trait("Category", "Expensive")]
-    public class SqlServer : BaseMigratorTests, IClassFixture<SqlServerFixture>
-    {
-        public SqlServer(SqlServerFixture fixture)
-        {
-            var adminConnectionString = fixture.Container.GetConnectionString();
-            AdminDb = new SqlConnection(adminConnectionString);
-            AdminDb.Execute(Sql["Db.Create"]);
-            var testConnectionString = adminConnectionString.Replace(MsSqlBuilder.DefaultDatabase, "test");
-            Db = new SqlConnection(testConnectionString);
-            Db.Open();
-        }
-    }
+    // TODO Fix pipeline issue:
+    // Docker.DotNet.DockerImageNotFoundException :
+    // Docker API responded with status code=NotFound, response={"message":"No such image: mcr.microsoft.com/mssql/server:latest"}
+    //[Trait("Category", "Expensive")]
+    //public class SqlServer : BaseMigratorTests, IClassFixture<SqlServerFixture>
+    //{
+    //    public SqlServer(SqlServerFixture fixture)
+    //    {
+    //        var adminConnectionString = fixture.Container.GetConnectionString();
+    //        AdminDb = new SqlConnection(adminConnectionString);
+    //        AdminDb.Execute(Sql["Db.Create"]);
+    //        var testConnectionString = adminConnectionString.Replace(MsSqlBuilder.DefaultDatabase, "test");
+    //        Db = new SqlConnection(testConnectionString);
+    //        Db.Open();
+    //    }
+    //}
 
-    public class SqlServerFixture : BaseDbFixture
-    {
-        public SqlServerFixture()
-        {
-            Container = new MsSqlBuilder()
-                .WithImage("mcr.microsoft.com/mssql/server:latest")
-                .Build();
-        }
-    }
+    //public class SqlServerFixture : BaseDbFixture
+    //{
+    //    public SqlServerFixture()
+    //    {
+    //        Container = new MsSqlBuilder()
+    //            .WithImage("mcr.microsoft.com/mssql/server:latest")
+    //            .Build();
+    //    }
+    //}
 }
